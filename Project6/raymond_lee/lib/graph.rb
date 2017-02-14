9ee6c45cb6cb4507ccaf5527ea800aa390a1ac11
Raymond Lee <raylee0525@gmail.com>
2017-02-13 23:21:40 -0800

    Complete dependency graph class

--- Project6/raymond_lee/lib/graph.rb
+++ Project6/raymond_lee/lib/graph.rb
@@ -1,15 +1,29 @@
 class Vertex
-  def initialize(value)
+  attr_reader :value, :in_edges, :out_edges
 
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
   end
 end
 
 class Edge
-  def initialize(from_vertex, to_vertex, cost = 1)
+  attr_reader :from_vertex, :to_vertex, :cost
 
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    @to_vertex.in_edges << self
+    @from_vertex.out_edges << self
   end
 
   def destroy!
+    @to_vertex.in_edges.pop
+    @from_vertex.out_edges.pop
 
+    @to_vertex = nil
+    @from_vertex = nil
   end
 end
