1ad2734e38046ee418966d105b59375d5370ca9f
Raymond Lee <raylee0525@gmail.com>
2017-02-14 19:47:56 -0800

    Complete Tarjan and Topological sort

--- Project6/raymond_lee/lib/graph.rb
+++ Project6/raymond_lee/lib/graph.rb
@@ -2,9 +2,7 @@ class Vertex
   attr_reader :value, :in_edges, :out_edges
 
   def initialize(value)
-    @value = value
-    @in_edges = []
-    @out_edges = []
+    @value, @in_edges, @out_edges = value, [], []
   end
 end
 
@@ -12,18 +10,16 @@ class Edge
   attr_reader :from_vertex, :to_vertex, :cost
 
   def initialize(from_vertex, to_vertex, cost = 1)
-    @from_vertex = from_vertex
-    @to_vertex = to_vertex
-    @cost = cost
-    @to_vertex.in_edges << self
+    @from_vertex, @to_vertex, @cost = from_vertex, to_vertex, cost
+
     @from_vertex.out_edges << self
+    @to_vertex.in_edges << self
   end
 
   def destroy!
-    @to_vertex.in_edges.pop
-    @from_vertex.out_edges.pop
+    @to_vertex.in_edges.delete(self)
+    @from_vertex.out_edges.delete(self)
 
-    @to_vertex = nil
-    @from_vertex = nil
+    @from_vertex, @to_vertex = nil, nil
   end
 end
--- Project6/raymond_lee/lib/topological_sort.rb
+++ Project6/raymond_lee/lib/topological_sort.rb
@@ -1,7 +1,54 @@
 require_relative 'graph'
+require 'set'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
+# Kahn's Algorithm
 def topological_sort(vertices)
+  count_hash = {}
+  sorted = []
+  queue = []
 
+  vertices.each do |vertex|
+    count_hash[vertex] = vertex.in_edges.count
+    queue << vertex if vertex.in_edges.empty?
+  end
+
+  until queue.empty?
+    current = queue.shift
+    sorted << current
+
+    current.out_edges.each do |edge|
+      vertex = edge.to_vertex
+      count_hash[vertex] -= 1
+
+      if count_hash[vertex] == 0
+        queue << vertex
+      end
+    end
+  end
+
+  sorted
+end
+
+# Tarjan's Algorithm
+def topological_sort2(vertices)
+  sorted = []
+  visited = Set.new
+
+  vertices.each do |vertex|
+    dfs(vertex, visited, sorted) unless visited.include?(vertex)
+  end
+
+  sorted
+end
+
+def dfs(vertex, visited, sorted)
+  visited.add(vertex)
+
+  vertex.out_edges.each do |edge|
+    dfs(edge.to_vertex, visited, sorted) unless visited.include?(edge.to_vertex)
+  end
+
+  sorted.unshift(vertex)
 end
