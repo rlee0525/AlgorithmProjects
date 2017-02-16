efb3b7e84f7701cc10e8b6fe297409d1701548a2
Raymond Lee <raylee0525@gmail.com>
2017-02-15 21:53:33 -0800

    almost complete priority map

--- Project7/raymond_lee/lib/dijkstra1.rb
+++ Project7/raymond_lee/lib/dijkstra1.rb
@@ -12,14 +12,13 @@ def dijkstra1(source)
   }
 
   until possible_paths.empty?
-    current = possible_paths.min_by { |_, v| v[:cost] }[0]
-    current_cost = possible_paths[current][:cost]
+    current, current_cost = possible_paths.min_by { |_, v| v[:cost] }
     shortest_paths[current] = possible_paths[current]
     possible_paths.delete(current)
     current.out_edges.each do |edge|
       next_vertex = edge.to_vertex
       possible_paths[next_vertex] = {
-        cost: edge.cost + current_cost,
+        cost: edge.cost + current_cost[:cost],
         previous_edge: current
       }
     end
--- Project7/raymond_lee/lib/priority_map.rb
+++ Project7/raymond_lee/lib/priority_map.rb
@@ -2,32 +2,52 @@ require_relative 'heap2'
 
 class PriorityMap
   def initialize(&prc)
+    @map = {}
+    @queue = BinaryMinHeap.new(&prc)
   end
 
   def [](key)
+    @map[key]
   end
 
   def []=(key, value)
+    insert(key, value)
+    @map[key] = value
   end
 
   def count
+    @queue.count
   end
 
   def empty?
+    @queue.empty?
   end
 
   def extract
+    key = @queue.extract
+    value = @map[key]
+    # update(key, value)
+    @map.delete(key)
+    [key, value]
+  end
+
+  def extract!
+    @queue.extract
   end
 
   def has_key?(key)
+    @map.key?(key)
   end
 
   protected
+
   attr_accessor :map, :queue
 
-  def insert(key, value)
+  def insert(key, _)
+    @queue.push(key) unless @map.key?(key)
   end
 
-  def update(key, value)
+  def update(key, _)
+    @queue.reduce!(key)
   end
 end
